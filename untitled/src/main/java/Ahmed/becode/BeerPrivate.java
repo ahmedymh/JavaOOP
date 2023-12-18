b245c39c62a6d2fffa8227502f9093b750b1d0b9
ahmedymh <ahmedbnz47@gmail.com>
2023-12-18 16:57:56 +0100

    exo 3

---
+++ untitled/src/main/java/Ahmed/becode/BeerPrivate.java
@@ -0,0 +1,42 @@
+package Ahmed.becode;
+
+public class BeerPrivate extends Beverage{
+    private String name;
+    private double alcoholPercentage;
+
+    public double getAlcoholPercentage() {
+        return alcoholPercentage;
+    }
+
+    public void setAlcoholPercentage(double alcoholPercentage) {
+        this.alcoholPercentage = alcoholPercentage;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String beerInfo() {
+        return "Hi, I'm " + name + " and have an alcohol percentage of " + alcoholPercentage + " and I have a light color.";
+    }
+
+    public BeerPrivate(String name, double alcoholPercentage) {
+        super();
+        this.name = name;
+        this.alcoholPercentage = alcoholPercentage;
+    }
+
+    public static void main(String[] args) {
+        BeerPrivate duvel = new BeerPrivate("Duvel", 8.5);
+        duvel.setColor("light");
+        duvel.setPrice(3.5);
+        System.out.println(duvel.getInfo());
+        System.out.println(duvel.getAlcoholPercentage());
+        System.out.println(duvel.getColor());
+        System.out.println(duvel.beerInfo());
+    }
+}
